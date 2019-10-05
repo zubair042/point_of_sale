37e9b04b08e1d03e838ec25a64770ed5fd28b65a
zubair042 <042zubair@.github.com>
2019-10-05 11:11:37 +0500

    commit

--- resources/views/accounts/add_account.blade.php
+++ resources/views/accounts/add_account.blade.php
@@ -7,6 +7,7 @@
 <script src="{{ asset('global_assets/js/plugins/forms/styling/uniform.min.js') }}"></script>
 <script src="{{asset('global_assets/js/plugins/forms/selects/select2.min.js')}}"></script>
 <script src="{{asset('global_assets/js/demo_pages/form_checkboxes_radios.js')}}"></script>
+<link href="{{asset('global_assets/css/icons/material/icons.css')}}" rel="stylesheet" type="text/css">
 
 <div class="row">
 	<div class="col-md-5">
@@ -20,6 +21,7 @@
 			<div class="card-body">
 				<form method="post" action="{{ route('save_accounts')}}" id="add_accounts_form">
 					{{ csrf_field() }}
+					<input type="hidden" name="counter" value="1">
 					<div class="row form-group">
 						<div class="col-md-3">
 							<span class="input-group-text">
@@ -27,7 +29,7 @@
 							</span>
 						</div>
 						<div class="col-md-9">
-							<input type="text" name="account_id" class="form-control" maxlength="8">
+							<input type="text" name="item_name" id="item_name" class="form-control" maxlength="8">
 						</div>
 					</div>
 					<div class="row text-center">
@@ -54,7 +56,7 @@
 							</span>
 						</div>
 						<div class="col-md-9">
-							<input type="number" name="" class="form-control">
+							<input type="number" name="quantity" id="quantity_input" class="form-control">
 						</div>
 					</div>
 					<div class="row foot_text" style="display: none;"> 
@@ -64,7 +66,7 @@
 							</span>
 						</div>
 						<div class="col-md-9">
-							<input type="number" name="" class="form-control">
+							<input type="number" name="foot" id="foot_input" class="form-control">
 						</div>
 					</div>
 					<div class="row">
@@ -74,7 +76,7 @@
 							</span>
 						</div>
 						<div class="col-md-9">
-							<input type="text" name="account_name" class="form-control">
+							<input type="text" name="price" id="price" class="form-control">
 						</div>
 					</div>
 					<div class="row">
@@ -84,7 +86,7 @@
 							</span>
 						</div>
 						<div class="col-md-9">
-							<input type="text" name="address" class="form-control">
+							<input type="text" name="discount" class="form-control">
 						</div>
 					</div>
 					<div class="row form-group">
@@ -94,14 +96,14 @@
 							</span>
 						</div>
 						<div class="col-md-9">
-							<input type="text" name="address2" class="form-control">
+							<input type="text" name="pending" class="form-control">
 						</div>
 					</div>
 					<div class="row text-center">
 						<div class="col-md-12">
 							<button type="submit" class="btn btn-success legitRipple"><i class="icon-checkmark mr-2"></i>Total</button>
 							<button type="button" class="btn btn-danger legitRipple" onclick="resetForm();"><i class="icon-reset mr-2"></i>Reset</button>
-							<button type="button" class="btn btn-primary legitRipple"><i class="icon-arrow-up-right32 mr-2"></i>Add Item</button>
+							<button type="button" onclick="add_item();" class="btn btn-primary legitRipple"><i class="icon-arrow-up-right32 mr-2"></i>Add Item</button>
 						</div>
 					</div>
 				</form>
@@ -129,15 +131,8 @@
 								<th>Action</th>
 							</tr>
 						</thead>
-						<tbody>
-							<tr>
-								<td>1</td>
-								<td>Eugene</td>
-								<td>Kopyov</td>
-								<td>@Kopyov</td>
-								<td>@Kopyov</td>
-								<td>@Kopyov</td>
-							</tr>
+						<tbody id="list_item_div">
+
 						</tbody>
 					</table>
 				</div>
@@ -163,6 +158,20 @@
 	      $('.quantity_text').hide();
 	   }
 	});
+
+	function add_item(){
+		var counter = $("input[name=counter]").val();
+		var name = $('#item_name').val();
+		var quantity = $('#quantity_input').val();
+		var foot = $('#foot_input').val();
+		var price = $('#price').val();
+		$('#list_item_div').append('<tr id="list_item_row_'+counter+'"><td>'+counter+'</td><td>'+name+'</td><td>'+quantity+'</td><td>'+foot+'</td><td>'+price+'</td><td><a href="javascript:;" onclick="remove('+counter+')" class="text-default font-weight-semibold letter-icon-title"><i class="mi-delete mr-3 mi-2x" style="color: red;"></i></a></td></tr>');
+		counter = parseInt(counter)+1;
+		$("input[name=counter]").val(counter);
+	}
+	function remove(id){
+		$('#list_item_row_'+id+'').remove();
+	}
 	function resetForm() {
 		//alert();
 		document.getElementById("add_accounts_form").reset();
